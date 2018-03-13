e927941fa61e01adc027244b17904b53b4371021
Sebastien Bramille <sebastien.bramille@gmail.com>
2018-03-13 13:49:21 +0000

    Add OpCode

---
+++ OpCode.js
@@ -0,0 +1,14 @@
+module.exports = {
+  0: 'Continuation frame',
+  1: 'Text frame',
+  2: 'Binary frame',
+  8: 'Connection close frame',
+  9: 'Ping frame',
+  10: 'Pong frame',
+  CONTINUATION: 0,
+  TEXT: 1,
+  BINARY: 2,
+  CONNECTION_CLOSE: 8,
+  PING: 9,
+  PONG: 10,
+};
